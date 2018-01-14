f1eb72a6f2366eb778d4ed70c09503a145e68c78
tuoli <huaqin.shq@alibaba-inc.com>
2018-01-14 15:51:29 +0800

    wigs.py设置依赖的工程路径

--- backend/project/project/wsgi.py
+++ backend/project/project/wsgi.py
@@ -8,8 +8,14 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0,PROJECT_DIR)
+
+print PROJECT_DIR
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings.prod")
 
