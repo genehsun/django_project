9cdcebf3add58893f50fe350fec0878622ff04ba
tuoli <huaqin.shq@alibaba-inc.com>
2018-01-14 14:47:09 +0800

    修改ALLOWED_HOSTS配置

--- backend/project/project/settings/prod.py
+++ backend/project/project/settings/prod.py
@@ -3,7 +3,7 @@ from base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'localhost', 'dev.myblog.com', '30.7.91.227']
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'localhost', 'dev.myblog.com', '30.7.91.227', '115.198.103.53', '192.168.16.105', '67.218.132.52']
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
